065fe31b460497cac42bd9e0913977ed8b6e32bb
Alisher <alisherlife03@gmail.com>
2026-07-24 15:26:28 +0500

    safety check for ClassEvent

--- backend/camphub/models.py
+++ backend/camphub/models.py
@@ -115,7 +115,7 @@ class ClassEvent(models.Model):
         Event, on_delete=models.CASCADE, null=True, blank=True, db_column='event_id')
     room_id = models.ForeignKey(
         Room, on_delete=models.CASCADE, null=True, blank=True, db_column='room_id')
-
+    
     def clean(self):
         super().clean()
 
@@ -135,24 +135,29 @@ class ClassEvent(models.Model):
         )
 
         # prevent self-collision
+        
         if self.pk:
             conflicts = conflicts.exclude(pk=self.pk)
+            
+        if self.cohort_id and self.cohort_id.study_year_id:
+                    current_study_year = self.cohort_id.study_year_id
+                    same_year_cohort_conflicts = conflicts.filter(
+                        cohort_id__study_year_id=current_study_year,
+                        cohort_id=self.cohort_id
+                    )
+                    if same_year_cohort_conflicts.exists():
+                        if self.room_id and conflicts.filter(room_id=self.room_id).exists():
+                            raise ValidationError(
+                                f"Conflict: Room {self.room_id} already has a {target_status} event during this time."
+                            )
 
-        if self.room_id and conflicts.filter(room_id=self.room_id).exists():
-            raise ValidationError(
-                f"Conflict: Room {self.room_id} already has a {target_status} event during this time."
-            )
+                        # Instructor Conflict
+                        if self.instructor_id and conflicts.filter(instructor_id=self.instructor_id).exists():
+                            raise ValidationError(
+                                f"Conflict: Instructor {self.instructor_id} is already busy with another {target_status} event."
+                            )
 
-        # Instructor Conflict
-        if self.instructor_id and conflicts.filter(instructor_id=self.instructor_id).exists():
-            raise ValidationError(
-                f"Conflict: Instructor {self.instructor_id} is already busy with another {target_status} event."
-            )
 
-        if self.cohort_id and conflicts.filter(cohort_id=self.cohort_id).exists():
-            raise ValidationError(
-                f"Conflict: Cohort {self.cohort_id} is already attending a {target_status} event."
-            )
 
 
 class MealTime(models.Model):
