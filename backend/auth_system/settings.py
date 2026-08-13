b971d1a62363e3680e591556210e8838e3f162a7
Alisher <alisherlife03@gmail.com>
2026-08-13 16:44:40 +0500

    Enhance UserCreateSerializer to include 'is_staff' field and set it as read-only; update Djoser settings to add 'current_user' serializer; modify permission classes in multiple viewsets to restrict actions to admins only; add migration for merging previous migrations.

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -214,6 +214,7 @@ DJOSER = {
     'SERIALIZERS': {
         'user_create': 'accounts.serializers.UserCreateSerializer',
         'user': 'accounts.serializers.UserCreateSerializer',
+        'current_user': 'accounts.serializers.UserCreateSerializer',
         'user_delete': 'djoser.serializers.UserDeleteSerializer',
     }
 }
